ae3bada9c6e79e12e964b4121cc18df832e6c571
Anatoly Samokisha <anatoly@samokisha.com>
2019-05-07 23:18:31 +0300

    Create expression class with basic functionality

---
+++ src/main/java/com/samokisha/calculatormateacademy/calculator/Expression.java
@@ -0,0 +1,34 @@
+package com.samokisha.calculatormateacademy.calculator;
+
+public class Expression {
+
+    private Operand leftOperand;
+    private Operand rightOperand;
+    private Operation operation;
+
+    private String result;
+
+    public void parseExpression(String task) {
+        if (task.contains("+")) {
+            operation = new PlusOperation();
+        } else if (task.contains("-")) {
+            operation = new MinusOperation();
+        } else if (task.contains("*")) {
+            operation = new MultiplyOperation();
+        } else if (task.contains("/")) {
+            operation = new DivideOperation();
+        }
+
+        String[] operands = task.split(operation.getSymbol());
+        leftOperand = new Operand(operands[0]);
+        rightOperand = new Operand(operands[1]);
+    }
+
+    public void performExpression() {
+        result = operation.eval(leftOperand, rightOperand);
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
