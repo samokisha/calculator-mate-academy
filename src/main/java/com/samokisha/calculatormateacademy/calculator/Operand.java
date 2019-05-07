1a0e2024fe0304241faa6240c7b429b1d79138c1
Anatoly Samokisha <anatoly@samokisha.com>
2019-05-07 23:21:43 +0300

    Create operand class with basic functionality

---
+++ src/main/java/com/samokisha/calculatormateacademy/calculator/Operand.java
@@ -0,0 +1,18 @@
+package com.samokisha.calculatormateacademy.calculator;
+
+public class Operand {
+
+    private String value;
+
+    public Operand(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
