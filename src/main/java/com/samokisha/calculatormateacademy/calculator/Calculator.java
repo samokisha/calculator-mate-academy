a45f88d194f42fc7df60dd9ec43109a01529dfc5
Anatoly Samokisha <anatoly@samokisha.com>
2019-05-07 22:45:32 +0300

    Create calculator class

---
+++ src/main/java/com/samokisha/calculatormateacademy/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package com.samokisha.calculatormateacademy.calculator;
+
+public class Calculator {
+
+    private String task;
+    private String result;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
